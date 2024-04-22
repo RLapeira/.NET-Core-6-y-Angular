f006999374222d0c0e0a5ff75c91551dd148d7b6
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-22 12:12:30 +0200

    Configuración Tabla Listado Macotas

--- FE-CRUDMascotas/src/app/components/listado-mascota/listado-mascota.component.ts
+++ FE-CRUDMascotas/src/app/components/listado-mascota/listado-mascota.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Mascota } from '../../interfaces/mascota';
+
+const ELEMENT_DATA: Mascota[] = [
+  { nombre: 'Onza', edad: 15, raza: 'Labrador', color: 'Dorado', peso: 25 },
+  { nombre: 'Mike', edad: 5, raza: 'Golder', color: 'Dorado', peso: 44 },
+  { nombre: 'Milton', edad: 6, raza: 'Golden', color: 'Dorado', peso: 37 },
+  { nombre: 'Bartolo', edad: 3, raza: 'Dogo Argentino', color: 'Blanco', peso: 60 },
+  { nombre: 'Aquiles', edad: 5, raza: 'Ovejero Aleman', color: 'Negro', peso: 67 },
+  { nombre: 'Homero', edad: 1, raza: 'Labrador', color: 'Negro', peso: 44 },
+  { nombre: 'Mark', edad: 1, raza: 'Callejero', color: 'Negro', peso: 25 }
+];
 
 @Component({
   selector: 'app-listado-mascota',
@@ -6,7 +17,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado-mascota.component.css']
 })
 export class ListadoMascotaComponent implements OnInit {
-
+  displayedColumns: string[] = ['nombre', 'edad', 'raza', 'color', 'peso'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
