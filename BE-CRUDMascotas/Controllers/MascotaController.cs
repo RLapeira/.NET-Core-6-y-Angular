e03af7a96f3d1dee1c7dc7872d5caeb13708423d
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-23 12:06:29 +0200

    Creación Controlador Mascota y EndPoint GetMascotas()

---
+++ BE-CRUDMascotas/Controllers/MascotaController.cs
@@ -0,0 +1,33 @@
+﻿using BE_CRUDMascotas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BE_CRUDMascotas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MascotaController : ControllerBase
+    {
+        public readonly AplicationDbContext _context;
+
+        public MascotaController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMascotas()
+        {
+            try
+            {
+                var listMascotas = await _context.Mascotas.ToListAsync();
+                return Ok(listMascotas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
