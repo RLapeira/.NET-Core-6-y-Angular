d98f171e08f198f7ce78a76c54e37d4d4074d573
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-23 09:46:40 +0200

    Hacer el Ruteo de los Distintos Componentes

--- FE-CRUDMascotas/src/app/app-routing.module.ts
+++ FE-CRUDMascotas/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Componentes
+import { ListadoMascotaComponent } from './components/listado-mascota/listado-mascota.component';
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'listaMascotas', pathMatch: 'full' },
+  { path: 'listaMascotas', component: ListadoMascotaComponent },
+  { path: 'agregarMascotas', component: AgregarEditarMascotaComponent },
+  { path: 'verMascota/:id', component: VerMascotaComponent },
+  { path: 'editarMascota/:id', component: AgregarEditarMascotaComponent },
+  { path: '**', redirectTo: 'listaMascotas', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
