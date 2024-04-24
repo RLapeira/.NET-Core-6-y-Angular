30b05ac461512891fb70e2cbfdafaa4db09f48e5
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-24 11:15:14 +0200

    FE - Añadir Funcionalidad al Formulario de Editar Mascota

--- FE-CRUDMascotas/src/app/components/agregar-editar-mascota/agregar-editar-mascota.component.ts
+++ FE-CRUDMascotas/src/app/components/agregar-editar-mascota/agregar-editar-mascota.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Mascota } from '../../interfaces/mascota';
 import { MascotaService } from 'src/app/services/mascota.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregar-editar-mascota',
@@ -12,12 +12,15 @@ import { Router } from '@angular/router';
 })
 export class AgregarEditarMascotaComponent implements OnInit {
   loading: boolean = false;
-  form: FormGroup
+  form: FormGroup;
+  id: number;
+  operacion: string = 'Agregar';
 
   constructor(private fb: FormBuilder,
     private _mascotaService: MascotaService,
     private _snackBar: MatSnackBar,
-    private router: Router) {
+    private router: Router,
+    private aRoute: ActivatedRoute) {
       this.form = this.fb.group({
         nombre: ['', Validators.required],
         raza: ['', Validators.required],
@@ -25,12 +28,33 @@ export class AgregarEditarMascotaComponent implements OnInit {
         edad: ['', Validators.required],
         peso: ['', Validators.required]
       })
+    this.id = Number(this.aRoute.snapshot.paramMap.get('id'));
   }
 
   ngOnInit(): void {
+    if(this.id != 0){
+      this.operacion = 'Editar';
+      this.obtenerMascota(this.id);
+    }
   }
 
-  agregarMascota() {
+  obtenerMascota(id: number) {
+    this.loading = true;
+    this._mascotaService.getMascota(id).subscribe(data => {
+      this.form.setValue({
+        nombre: data.nombre,
+        raza: data.raza,
+        color: data.color,
+        edad: data.edad,
+        peso: data.peso
+      });
+      // Para que no dé error el setValue tienes que pasarle al formulario todos los atributos,
+      // si solo quieres unos cuantos, se utiliza el this.form.patchValue({ })
+      this.loading = false;
+    })
+  }
+
+  agregarEditarMascota() {
     //const nombre = this.form.get('nombre')?.value;
 
     const mascota: Mascota = {
@@ -41,15 +65,33 @@ export class AgregarEditarMascotaComponent implements OnInit {
       peso: this.form.value.peso
     }
 
-    // Enviamos objeto al back-end:
-    this._mascotaService.addMascota(mascota).subscribe(() => {
-      this.mensajeExito();
+    if(this.id != 0){
+      mascota.id = this.id;
+      this.editarMascota(this.id, mascota);
+    } else {
+      this.agregarMascota(mascota);
+    }
+  }
+
+  editarMascota(id: number, mascota: Mascota) {
+    this.loading = true;
+    this._mascotaService.updateMascota(id, mascota).subscribe(() => {
+      this.loading = false;
+      this.mensajeExito('actualizada');
       this.router.navigate(['/listaMascotas']);
     })
   }
 
-  mensajeExito() {
-    this._snackBar.open('La mascota fue registrada con exito', '', {
+  agregarMascota(mascota: Mascota) {
+    // Enviamos objeto al back-end:
+    this._mascotaService.addMascota(mascota).subscribe(() => {
+      this.mensajeExito('registrada');
+      this.router.navigate(['/listaMascotas']);
+    })
+  }
+
+  mensajeExito(texto: string) {
+    this._snackBar.open(`La mascota fue ${texto} con exito`, '', {
       duration: 4000,
       horizontalPosition: 'center',
       //verticalPosition: 'top'
